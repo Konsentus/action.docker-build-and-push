5322c5f84d58b8d3d2dee4091a2d6aa92030b50d
odellt <tom.odell@konsentus.com>
2019-10-30 14:34:04 +0000

    :tada: Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM alpine:latest
+FROM docker:latest
 
 ADD entrypoint.sh /entrypoint.sh
 
-RUN apk add --no-cache bash
+RUN apk add --no-cache bash \
+  && apk add --no-cache python3 \
+  && pip3 install awscli
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-
+set -e
 ## Standard ENV variables provided
 # ---
 # GITHUB_ACTION=The name of the action
@@ -18,5 +18,35 @@
 # see https://help.github.com/en/articles/development-tools-for-github-actions#logging-commands
 # echo ::set-output name={name}::{value}
 # -- DONT FORGET TO SET OUTPUTS IN action.yml IF RETURNING OUTPUTS
+
+assume_role() {
+  echo "Assuming role"
+  CREDS=$(aws sts assume-role --role-arn "arn:aws:iam::${AWS_ACCOUNT_ID}:role/${AWS_ACCOUNT_ROLE}" --role-session-name ami-builder --output json)
+
+  export AWS_ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY
+  export AWS_SESSION_TOKEN
+  export AWS_DEFAULT_REGION=${AWS_REGION}
+
+  AWS_ACCESS_KEY_ID=$(jq -r .Credentials.AccessKeyId <<< ${CREDS})
+  AWS_SECRET_ACCESS_KEY=$(jq -r .Credentials.SecretAccessKey <<< ${CREDS})
+  AWS_SESSION_TOKEN=$(jq -r .Credentials.SessionToken <<< ${CREDS})
+}
+
+assume_role
+
+$(aws ecr get-login --no-include-email --region $AWS_REGION)
+
+image_name="$AWS_ACCOUNT_ID.dkr.ecr.eu-west-2.amazonaws.com/$GITHUB_REPOSITORY"
+image_id=$(docker image build -q --no-cache . | cut -d':' -f2)
+docker tag "${image_id}" "${image_name}:${GITHUB_REF}"
+docker push "${image_name}:${GITHUB_REF}"
+
+# TODO investigate passing extra tags
+# for tag in "${_arg_tag[@]}"
+# do
+# 	 docker tag "${image_id}" "${image_name}:${tag}"
+# 	 docker push "${image_name}:${tag}"
+# done
 
 # exit with a non-zero status to flag an error/failure
